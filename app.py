8351614b9bd6a1a4e25dc28b5d9a354af5695bca
ParkJeonghunn <96563138+ParkJeonghunn@users.noreply.github.com>
2021-12-23 20:03:50 +0900

    21.12.17(1st week 5th homework3)
    Develop Movie Recommend Page

--- app.py
+++ app.py
@@ -1,33 +1,60 @@
 import certifi
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.p2cn0.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
 db = client.dbsparta
 
+import requests
+from bs4 import BeautifulSoup
+
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/mars", methods=["POST"])
-def web_mars_post():
-    name_receive = request.form['name_give']
-    address_receive = request.form['address_give']
-    size_receive = request.form['size_give']
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    image = og_image['content']
+    title = og_title['content']
+    description = og_description['content']
+
     doc = {
-        'name' : name_receive,
-        'address' : address_receive,
-        'size' : size_receive
+        'image': image,
+        'title': title,
+        'desc': description,
+        'star': star_receive,
+        'comment': comment_receive
     }
-    db.mars.insert_one(doc)
 
-    return jsonify({'msg': '주문 완료!'})
+    db.movies.insert_one(doc)
 
-@app.route("/mars", methods=["GET"])
-def web_mars_get():
-    order_list = list(db.mars.find({}, {'_id': False}))
-    return jsonify({'orders': order_list})
+    return jsonify({'msg': 'POST 연결 완료!'})
+
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movies_list = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'movies': movies_list})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
